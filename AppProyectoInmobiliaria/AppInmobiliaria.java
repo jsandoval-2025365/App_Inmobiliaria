389b55fa6e533104c93bc8475dee78cd2b60015b
jsandoval-2025365 <jsandoval-2025365@kinal.edu.gt>
2026-02-06 21:22:53 -0600

    feat: Se ha agregadó el archivo index AppInmobiliaria.java ademas se ha incluido la clase AppInmobiliaria junto con su static void main y su método start con sus clases abstractas

---
+++ AppProyectoInmobiliaria/AppInmobiliaria.java
@@ -0,0 +1,25 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AppInmobiliaria extends Application{
+
+    
+    public static void main(String[] args) {
+
+
+
+
+
+    
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+      
+      
+    }
+
+
+
+
+}
